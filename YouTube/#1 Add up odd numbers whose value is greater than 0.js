ad2a11507934a2f7c44d6b261936d4313f65cd47
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2020-12-14 15:53:56 +0900

    upd

--- YouTube/#1 Add up odd numbers whose value is greater than 0.js
+++ YouTube/#1 Add up odd numbers whose value is greater than 0.js
@@ -11,3 +11,24 @@ const calculate = (arr) => {
 
 // input = [5, 18, 2, -7, 88, 20, 109]
 // expected output = 114
+
+// OR
+const calculate = (arr) => {
+  return arr.reduce((accumulate, element) => {
+    if (element > 0 && element % 2 !== 0) {
+      accumulate += element;
+    }
+    return accumulate;
+  }, 0);
+};
+
+
+// OR
+const calculate = (arr) => {
+  return arr.reduce(
+    (accumulate, element) =>
+      element > 0 && element % 2 !== 0 ? accumulate + element : accumulate,
+    0
+  );
+};
+
