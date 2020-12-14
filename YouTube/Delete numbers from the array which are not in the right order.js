b5597132e3687ccc0c4ab30015ab25f0570c7d50
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2020-12-14 15:24:49 +0900

    upd

--- YouTube/Delete numbers from the array which are not in the right order.js
+++ YouTube/Delete numbers from the array which are not in the right order.js
@@ -2,7 +2,7 @@ function sort(arr) {
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] < arr[i - 1]) {
       arr.splice(i, 1);
-      // Since one element is being delete from the array we need to substract 1 from the index.
+      // Since one element is being deleted from the array we need to substract 1 from the index.
       i--;
     }
   }
