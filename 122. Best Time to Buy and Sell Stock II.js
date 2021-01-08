911987ea46f80677267b06aa64bdf7962be8b049
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2021-01-08 10:05:59 +0900

    initial

--- 122. Best Time to Buy and Sell Stock II.js
+++ 122. Best Time to Buy and Sell Stock II.js
@@ -1,7 +1,7 @@
 const maxProfit = (prices) => {
     let profit = 0;
     
-    // i = 0, because we are comparing the second number with the
+    // i = 1, because we are comparing the second number with the
     // first number(the number on the left).
     for (let i = 1; i < prices.length; i++) {
         if (prices[i] > prices[i -1]) {
