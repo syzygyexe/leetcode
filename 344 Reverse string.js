7dc09091013716ac3a4ed29e0a78da27a13845a9
Alexander Borisov <67188470+syzygyexe@users.noreply.github.com>
2021-01-09 12:31:05 +0900

    initial

---
+++ 344 Reverse string.js
@@ -0,0 +1,12 @@
+const reverseString = (s) => {
+    let start = 0;
+    let end = s.length - 1;
+    
+    while(start < end) {
+        let temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+};
